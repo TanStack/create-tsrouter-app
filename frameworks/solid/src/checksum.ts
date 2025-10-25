5d05aad55763557cd3d7239ab6d96f7fa449bd46
Brenley Dueck <brenleydueck@gmail.com>
2025-10-25 12:56:29 -0500

    fix: make solid work with start again (#213)

--- frameworks/solid/src/checksum.ts
+++ frameworks/solid/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = '82c4550425f4f669cf913e75164cbfdc8b1b64e2afe548339297eeb6c9e581c9'
+export const contentChecksum = '4db9b64a6be7a440ed6fa534528bd1b1f9256f1039d02a446f4612c4c4605e2a'
