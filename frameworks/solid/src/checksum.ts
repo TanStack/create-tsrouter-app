88ea4e40bdf418b8f29f0db2b74c19fa3d4ed635
Birk Skyum <birk.skyum@pm.me>
2025-10-25 16:32:38 +0200

    update snapshots

--- frameworks/solid/src/checksum.ts
+++ frameworks/solid/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = 'c7f773b3a7dfae74eefe6c1aabefef523827058f72cc9dc2f8c37a3df3d0933d'
+export const contentChecksum = '82c4550425f4f669cf913e75164cbfdc8b1b64e2afe548339297eeb6c9e581c9'
