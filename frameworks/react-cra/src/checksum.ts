f2a454145589e511904be615dd934c883fc66f37
Jack Herrington <jherr@pobox.com>
2025-10-30 14:05:27 -0700

    fix: adding react compiler

--- frameworks/react-cra/src/checksum.ts
+++ frameworks/react-cra/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = 'a764212c22f02af2f32bfd84918a2af90df10112b5ffaedfc825db426d615b1c'
+export const contentChecksum = '9955097b6723f7743c47d4ab8baa3100fddb88f3ab2f53c82666573b02934250'
