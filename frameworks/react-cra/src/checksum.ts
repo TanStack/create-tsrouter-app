453a72f6c4b008f1cac3cf46b78e2f0b1a15ee8c
Tanner Linsley <tannerlinsley@users.noreply.github.com>
2025-10-31 01:00:41 +0000

    release: v0.34.15

--- frameworks/react-cra/src/checksum.ts
+++ frameworks/react-cra/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = '9955097b6723f7743c47d4ab8baa3100fddb88f3ab2f53c82666573b02934250'
+export const contentChecksum = 'b8aed4bd3da4fd99c4df983c4bf13aaeabc3f044035f5b2849a4075ccb244ce6'
