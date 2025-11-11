bcf8b31ed3c621aec7650f04e0ef1201e9880166
Pooya Parsa <pooya@pi0.io>
2025-11-11 19:19:30 +0100

    update checksums

--- frameworks/react-cra/src/checksum.ts
+++ frameworks/react-cra/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = '4e120a677f2d05a2dbe492f761bd3fefc02b835ebcdf9511a9ed78ceaa545a41'
+export const contentChecksum = '22415a1abc64758541fdba06e79f1b24860e8100379e3a08c2cd0084f5ec26f9'
--- frameworks/solid/src/checksum.ts
+++ frameworks/solid/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = '8e60d2780707119865b8f8ba4274517e4a66391a2f2f62090d954b63716c2a01'
+export const contentChecksum = '0c2e44dacd37af5293071c9f8ea934a2b52c4e37abc13d3e4c811187c40cb2d3'
