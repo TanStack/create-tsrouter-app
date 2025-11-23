1a890ca8e34bdb562db14cc504b503c076f40dd0
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-22 23:46:09 -0800

    Update checksum.ts

--- frameworks/react-cra/src/checksum.ts
+++ frameworks/react-cra/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = 'a42905c8800e56f1a97cdfc380ba4543e2d296e1fde95246036a4eacdaa694d7'
+export const contentChecksum = '6210a2229c4479e93bdcfbd5ee67da83271365075e82eb8ba1a913c5b6224866'
