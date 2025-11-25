d43b2d47ddb1b1ad0f065b7071e12838179cfda0
Daniel Griesser <daniel.griesser.86@gmail.com>
2025-11-25 10:46:56 +0100

    checksum?

--- frameworks/react-cra/src/checksum.ts
+++ frameworks/react-cra/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = 'a42905c8800e56f1a97cdfc380ba4543e2d296e1fde95246036a4eacdaa694d7'
+export const contentChecksum = 'a2932d45e0b8b23cc8ea876ec00379239a4dbacc8f01dbe99a4f6bf68bffe6b4'
