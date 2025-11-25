23d0ece0034ecb0e92ef96a7c9692ad5d2d06e8a
Jack Herrington <jherr@pobox.com>
2025-11-25 09:10:27 -0800

    fix: checksum

--- frameworks/react-cra/src/checksum.ts
+++ frameworks/react-cra/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = 'b1b304034569c6d53f6f318b3e0927c52fd96d71939e21bc455e0c56ba9dbb86'
+export const contentChecksum = 'ed82e6b4b77aaf2d3ded6c3002d319c79eb68328b4e6ad1333211f106a978482'
