e235526ac8f3b7033ae94bbbe6d36e07517435d7
Jack Herrington <jherr@pobox.com>
2025-11-25 09:47:49 -0800

    fix: snapshots and checksums

--- frameworks/solid/src/checksum.ts
+++ frameworks/solid/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = '0c2e44dacd37af5293071c9f8ea934a2b52c4e37abc13d3e4c811187c40cb2d3'
+export const contentChecksum = 'd7da25588d0808bff386271b8ffabaf2124ec31ff1a8a5ea52b45b1336f59114'
