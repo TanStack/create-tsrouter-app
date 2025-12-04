696105f78325de02e6dd4af7cd01377a019e313b
Jack Herrington <jherr@pobox.com>
2025-12-04 13:42:32 -0800

    fix: bumping version

--- frameworks/react-cra/src/checksum.ts
+++ frameworks/react-cra/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = '306bf57f2816d4e859ecc2b5bd8e03c33aa6a8c7735d2be2656f98dc8a585cbf'
+export const contentChecksum = '62e2924262651009287ab81f0dfb686aae34c9b8aaee0cea101121822530b06a'
