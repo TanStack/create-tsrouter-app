71b694b7bb53cdd2b3ae2d42c38e76ae7c71b788
Jack Herrington <jherr@pobox.com>
2025-12-15 13:31:23 -0800

    fix: Netlify updates

--- frameworks/react-cra/src/checksum.ts
+++ frameworks/react-cra/src/checksum.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 // Generated from add-ons, examples, hosts, project, and toolchains directories
-export const contentChecksum = '33ec3ec7dbdb94bb6ef6d8ba9831b43fba7f3d1f88a10408684e0ba39d282142'
+export const contentChecksum = '91ab333689da4a55dac5572c70c66bf3cf8710caf4fe62ee5fd63d7fa924f8a2'
